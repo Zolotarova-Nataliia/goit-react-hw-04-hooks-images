3303242065983b4c0778257de4918463a3ac369a
Zolotarova-Nataliia <zolotarova.nataliia@gmail.com>
2022-01-27 21:41:25 +0200

    [fix] fix some styles

--- src/components/Gallery/ImageGallery/GalleryItem.styled.jsx
+++ src/components/Gallery/ImageGallery/GalleryItem.styled.jsx
@@ -1,16 +1,17 @@
 import styled from 'styled-components';
 
 export const GalleryItem = styled.li`
+  width: 24vw;
   height: 300px;
-  width: 450px;
 `;
 
 export const GalleryImage = styled.img`
   display: block;
   width: 100%;
   height: 100%;
+  object-fit: cover;
   transition: 0.3s;
   &:hover {
-    transform: scale(1.1);
+    transform: scale(0.9);
   }
 `;
--- src/components/Gallery/ImageGallery/ImageGallery.styled.jsx
+++ src/components/Gallery/ImageGallery/ImageGallery.styled.jsx
@@ -4,6 +4,6 @@ export const GalleryList = styled.ul`
   display: grid;
   grid-template-columns: repeat(4, 1fr);
   grid-gap: 1em;
-  padding: 20px;
   list-style: none;
+  padding-top: 20px;
 `;
